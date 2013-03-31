9ee27344ddf8ca1dac6af7b84e1057f3b997b21c
Brandon Lerner <lernerbrandon@gmail.com>
2013-03-30 20:51:39 -0400

    added footer contact us page, fixed css of buttons and index page

--- footer.php
+++ footer.php
@@ -2,29 +2,21 @@
 	</div><!-- close #mainContent-->
 </div> <!-- close #pageWrapper -->
 <footer>
-	<div id="footerWrapper">
+	<div id="footerWrapper" class="cf">
+		<div style="float:right">
+			<p>Follow Us</p>
+			<div class="addthis_toolbox addthis_32x32_style addthis_default_style">
+			    <a class="addthis_button_facebook_follow" addthis:userid="steamphotoshare"></a>
+			    <a class="addthis_button_twitter_follow" addthis:userid="iOSDota"></a>
+			    <a class="addthis_button_tumblr_follow" addthis:userid="lernerb"></a>
+			</div>
+			<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-515731c3301753b5"></script>
+		</div>
+
+		<a href="/contact.php">Contact Us</a><br>	
+		<a href="/sitemap.php">Sitemap</a><br>
 		<a href="http://steampowered.com">Powered by Steam&trade;</a>
 
-		<p>Follow Us</p>
-		<div class="addthis_toolbox addthis_32x32_style addthis_default_style">
-		    <a class="addthis_button_facebook_follow" addthis:userid="steamphotoshare"></a>
-		    <a class="addthis_button_twitter_follow" addthis:userid="iOSDota"></a>
-		    <a class="addthis_button_tumblr_follow" addthis:userid="lernerb"></a>
-		</div>
-		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-515731c3301753b5"></script>
-
-
-		<a href="./sitemap.php">Sitemap</a>
-
-
-		<?php 
-
-		try {
-		    echo 'User ' . ($openid->isValid ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
-
-		} catch(ErrorException $e) {
-		    echo $e->getMessage();
-		}?>
 	</div>
 </footer>
 </body>
--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 include("header.php");
 ?>
 
-woopwoopwoop
+<div class="img" style="float:right;background:#444;padding:5px;">
+	<img src="/img/sits_large_border.png" alt="Log In With Steam"/>
+</div>
+
+<p>
+	Welcome to Steam Photo Share. Here you can login with steam and 
+	share photos from your favorite moments in-game.
+</p>
+
+<p>
+	You must sign in with a Steam Community ID to upload, comment and rate photos. 
+</p>
+
+
+
+
 <?php
 
 
